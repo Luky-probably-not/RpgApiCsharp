11912449c99c5e620c6edec37c4868ebf4dd1737
jeanbaptiste-bodusseau <bodusseau.jb@gmail.com>
2025-07-17 17:04:58 +0200

    m

--- Jeu/Program.cs
+++ Jeu/Program.cs
@@ -1,3 +1,7 @@
 ﻿using Modeles.FonctionsJeu;
 
 GameManager.Instance.Debut(5).GetAwaiter().GetResult();
+foreach (var kvp in GameManager.Instance.expedition.Sac)
+{
+    Console.WriteLine("cle : {0}; value : {1}",kvp.Key, kvp.Value);
+}
